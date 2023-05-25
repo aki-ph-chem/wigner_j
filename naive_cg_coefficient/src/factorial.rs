c20948e2e91acdbd65e45cfa5e129346572d96c9
aki <aki@mymail.com>
2023-05-25 10:53:26 +0900

    Changed implementation of factorial from recursion to loop.

--- naive_cg_coefficient/src/factorial.rs
+++ naive_cg_coefficient/src/factorial.rs
@@ -3,16 +3,56 @@
 //!
 
 /// Implemented by recursive functions  
-pub fn factorial(n: i64) -> i64 {
+pub fn _factorial(n: i64) -> i64 {
     match n {
         0 => 1,
         1 => 1,
-        _ => n * factorial(n - 1),
+        _ => n * _factorial(n - 1),
     }
 }
 
 #[cfg(test)]
 mod tests_factorial{
+    use super::*;
+
+    #[test]
+    fn test_factorial_0() {
+        let factorial_0 = 1;
+        let factorial_1 = 1;
+
+        assert_eq!(factorial_0, _factorial(0));
+        assert_eq!(factorial_1, _factorial(1));
+    }
+
+    #[test]
+    fn test_factorial_n() {
+        let factorial_3 = 6;
+        assert_eq!(factorial_3, _factorial(3));
+
+        let factorial_4 = 4 * 6;
+        assert_eq!(factorial_4, _factorial(4));
+
+        let factorial_6 = 6 * 5 * factorial_4;
+        assert_eq!(factorial_6, _factorial(6));
+    }
+}
+
+/// Implemented by 'for loop' 
+//pub fn factorial_loop(n: i64) -> i64 {
+pub fn factorial(n: i64) -> i64 {
+    if n == 0{
+        1
+    } else {
+        let mut tmp = 1;
+        for k in 1 .. n + 1 {
+            tmp *= k;
+        } 
+        tmp
+    }
+}
+
+#[cfg(test)]
+mod tests_factorial_loop{
     use super::*;
 
     #[test]
@@ -34,45 +74,6 @@ mod tests_factorial{
 
         let factorial_6 = 6 * 5 * factorial_4;
         assert_eq!(factorial_6, factorial(6));
-    }
-}
-
-/// Implemented by 'for loop' 
-pub fn factorial_loop(n: i64) -> i64 {
-    if n == 0{
-        1
-    } else {
-        let mut tmp = 1;
-        for k in 1 .. n + 1 {
-            tmp *= k;
-        } 
-        tmp
-    }
-}
-
-#[cfg(test)]
-mod tests_factorial_loop{
-    use super::*;
-
-    #[test]
-    fn test_factorial_0() {
-        let factorial_0 = 1;
-        let factorial_1 = 1;
-
-        assert_eq!(factorial_0, factorial_loop(0));
-        assert_eq!(factorial_1, factorial_loop(1));
-    }
-
-    #[test]
-    fn test_factorial_n() {
-        let factorial_3 = 6;
-        assert_eq!(factorial_3, factorial_loop(3));
-
-        let factorial_4 = 4 * 6;
-        assert_eq!(factorial_4, factorial_loop(4));
-
-        let factorial_6 = 6 * 5 * factorial_4;
-        assert_eq!(factorial_6, factorial_loop(6));
     }
 }
 
