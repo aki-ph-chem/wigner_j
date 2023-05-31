3a6e0efdee55cbc6e5900c66859b6a596c59d9d4
aki <aki@mymail.com>
2023-06-01 01:47:08 +0900

    add examples of C-G coefficient

--- naive_cg_coefficient/examples/cg_coefficient_example.rs
+++ naive_cg_coefficient/examples/cg_coefficient_example.rs
@@ -3,18 +3,27 @@ use naive_cg_coefficient::cg_coeffcient as cg;
 
 fn main(){
     let cg_1 = cg::CGCoefficient::new(2,2,3,2,5,4);
+    cg_1.show_list();
     let res_cg_1 = cg_1.calc_value();
     println!("res_cg_1 = {}", res_cg_1);
 
     let cg_2 = cg::CGCoefficient::new(2,1,1,1,3, 2);
+    cg_2.show_list();
     let res_cg_2 = cg_2.calc_value();
     println!("res_cg_2 = {}", res_cg_2);
 
     let cg_3 = cg::CGCoefficient::new(12,11,11,11,13,12);
+    cg_3.show_list();
     let res_cg_3 = cg_3.calc_value();
     println!("res_cg_3 = {}", res_cg_3);
 
     let cg_4 = cg::CGCoefficient::new(6,3,2,1,8,4);
+    cg_4.show_list();
     let res_cg_4 = cg_4.calc_value();
     println!("res_cg_4 = {}", res_cg_4);
+
+    let cg_5 = cg::CGCoefficient::new(6,0,4,0,2,0);
+    cg_5.show_list();
+    let value_5 = cg_5.calc_value();
+    println!("res_cg_5 = {}", value_5);
 }
