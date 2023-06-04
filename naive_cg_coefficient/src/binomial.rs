40f770cd4a0753fe80f1ad8de1c4c627060ec0aa
aki <aki@mymail.com>
2023-06-05 00:34:38 +0900

    refactor the code

--- naive_cg_coefficient/src/binomial.rs
+++ naive_cg_coefficient/src/binomial.rs
@@ -1,8 +1,9 @@
-pub fn binomial_rec(n: i64, k: i64) -> i64 {
+/// calculate binomial by recursion
+pub fn binomial(n: i64, k: i64) -> f64 {
     if n == k || k == 0 {
-        1
+        1.0
     } else{
-        binomial_rec(n - 1, k -1) + binomial_rec(n - 1, k)
+        binomial(n - 1, k -1) + binomial(n - 1, k)
     }
 }
 
@@ -12,33 +13,34 @@ mod tests_binomial_recursive {
 
     #[test]
     fn test_binomial_10() {
-        assert_eq!(10, binomial_rec(10,1));
-        assert_eq!(1, binomial_rec(3,0));
+        assert!((10.0 - binomial(10,1).abs() < std::f64::EPSILON));
+        assert!((1.0 - binomial(3,0)).abs() < std::f64::EPSILON);
     }
 
     #[test]
     fn test_binomial_100() {
-        assert_eq!(100, binomial_rec(100,1));
-        assert_eq!(1, binomial_rec(100,0));
+        assert!((100.0 - binomial(100,1).abs() < std::f64::EPSILON));
+        assert!((1.0 - binomial(100,0).abs() < std::f64::EPSILON));
     }
 
     #[test]
     fn test_binomial_5() {
-        let ans = [1,5,10,10,5,1];
+        let ans = [1.0,5.0,10.0,10.0,5.0,1.0];
         for i in 0..ans.len() {
-            assert_eq!(ans[i], binomial_rec(5, i as i64));
+            assert!((ans[i] - binomial(5, i as i64)).abs() < std::f64::EPSILON);
         }
     }
 
     #[test]
     fn test_binomial_6() {
-        let ans = [1,6,15,20,15,6,1];
+        let ans = [1.0,6.0,15.0,20.0,15.0,6.0,1.0];
         for i in 0..ans.len() {
-            assert_eq!(ans[i], binomial_rec(6, i as i64));
+            assert!((ans[i] - binomial(6, i as i64)).abs() < std::f64::EPSILON);
         }
     }
 } 
 
+/// calculate binomial by dp 
 pub fn binomial_dp(n: i64, k: i64, array: &mut Vec<Vec<i64>>){
     for i in 0..n + 1 {
         for j in 0..std::cmp::min(i,k) + 1 {
