10c1246a51b48b7347d4fddc63552c68fa9d788f
aki <aki@mymail.com>
2023-06-16 11:02:38 +0900

    fix code sytle and indent

--- naive_cg_coefficient/src/internal.rs
+++ naive_cg_coefficient/src/internal.rs
@@ -20,12 +20,12 @@ pub fn sign(n: i32) -> f64 {
 pub fn is_triangle(j_1: i64, j_2: i64, j_3: i64,
                    m_1: i64, m_2: i64, m_3: i64) -> bool {
 
-        let (j_min, j_max) = (j_1 + j_2, (j_1 - j_2).abs());
+    let (j_min, j_max) = (j_1 + j_2, (j_1 - j_2).abs());
 
-        let is_triangle_m = m_3 != m_1 + m_2;
-        let is_traianble_j = j_3 < j_min || j_max < j_3;
+    let is_triangle_m = m_3 != m_1 + m_2;
+    let is_traianble_j = j_3 < j_min || j_max < j_3;
 
-        is_triangle_m && is_traianble_j
+    is_triangle_m && is_traianble_j
 }
 
 /// calculation of triangle factor
@@ -35,92 +35,91 @@ pub fn is_triangle(j_1: i64, j_2: i64, j_3: i64,
 /// ```
 ///
 pub fn delta(j_1: i64, j_2: i64, j_3: i64) -> f64{
-        ((2 * j_3 + 1) as f64) 
-            *(factorial::factorial(j_3 + j_1 - j_2) as f64)
-            *(factorial::factorial(j_3 - j_1 + j_2) as f64)
-            *(factorial::factorial(j_1 + j_2 - j_3) as f64)
-            /(factorial::factorial(j_3 + j_1 + j_2 + 1) as f64)
+    ((2 * j_3 + 1) as f64) 
+        *(factorial::factorial(j_3 + j_1 - j_2) as f64)
+        *(factorial::factorial(j_3 - j_1 + j_2) as f64)
+        *(factorial::factorial(j_1 + j_2 - j_3) as f64)
+        /(factorial::factorial(j_3 + j_1 + j_2 + 1) as f64)
 }
 
 /// show list of j_1, j_1, j_3, m_1, m_2, m_3
 /// for clebsch-gordan coefficient
 pub fn show_list(j_1: i64, j_2: i64, j_3: i64,
-                   m_1: i64, m_2: i64, m_3: i64) {
-
-        println!("<{},{},{},{}|{},{} >",
-                 j_1, m_1, j_2, m_2, j_3, m_3);
+                 m_1: i64, m_2: i64, m_3: i64) {
+    println!("<{},{},{},{}|{},{} >",
+             j_1, m_1, j_2, m_2, j_3, m_3);
 } 
 
 /// show list of j_1, j_1, j_3, m_1, m_2, m_3
 /// for wigner 3j-symbol
 pub fn show_list_3j(j_1: i64, j_2: i64, j_3: i64,
-                   m_1: i64, m_2: i64, m_3: i64) {
-
-        println!("( {} ,{}, {} )\n( {}, {}, {} ) "
-                 ,j_1, j_2, j_3, m_1, m_2, m_3);
+                    m_1: i64, m_2: i64, m_3: i64) {
+    println!("( {} ,{}, {} )\n( {}, {}, {} ) "
+             ,j_1, j_2, j_3, m_1, m_2, m_3);
 } 
 
 /// calculate Clebsch-Gordan coefficient
 pub fn calc_cg_raw(j_1: i64, j_2: i64, j_3: i64,
                    m_1: i64, m_2: i64, m_3: i64) -> f64 {
 
-        if is_triangle(j_1, j_2, j_3, m_1, m_2, m_3) {
-            return 0.0;
-        }
-
-        let s = factorial::factorial(j_3 + m_3) 
-            * factorial::factorial(j_3 - m_3);
-        let s_1 = factorial::factorial(j_1 + m_1)
-            * factorial::factorial(j_1 - m_1);
-        let s_2 = factorial::factorial(j_2 + m_2)
-            * factorial::factorial(j_2 - m_2);
-        
-        let s = ((s * s_1 * s_2) as f64).powf(0.5);
-
-        let k_max = std::cmp::min( 
-            std::cmp::min(j_1 + j_2 - j_3, j_1 - m_1), j_2 + m_2);
-        let k_min = std::cmp::max(std::cmp::max(-j_3 + j_2 - m_1, -j_3 + j_1 + m_2), 0);
-
-        let mut res_cg = 0.0;
-        for k in k_min .. k_max + 1 {
-            let k_1 = factorial::factorial(j_1 + j_2 - j_3 - k);
-            let k_2 = factorial::factorial(j_1 - m_1 - k);
-            let k_3 = factorial::factorial(j_2 + m_2- k);
-            let k_4 = factorial::factorial(j_3 - j_2 + m_1 + k);
-            let k_5 = factorial::factorial(j_3 - j_1 - m_2 + k);
-
-            res_cg += sign(k as i32)/(factorial::factorial(k) * k_1 * k_2 * k_3 * k_4 * k_5);
-        }
-
-        delta(j_1, j_2, j_3).powf(0.5) * s * res_cg 
+    if is_triangle(j_1, j_2, j_3, m_1, m_2, m_3){
+        return 0.0;
     }
+
+    let s = factorial::factorial(j_3 + m_3) 
+        * factorial::factorial(j_3 - m_3);
+    let s_1 = factorial::factorial(j_1 + m_1)
+        * factorial::factorial(j_1 - m_1);
+    let s_2 = factorial::factorial(j_2 + m_2)
+        * factorial::factorial(j_2 - m_2);
+
+    let s = ((s * s_1 * s_2) as f64).powf(0.5);
+
+    let k_max = std::cmp::min( 
+        std::cmp::min(j_1 + j_2 - j_3, j_1 - m_1), j_2 + m_2);
+    let k_min = std::cmp::max(std::cmp::max(-j_3 + j_2 - m_1, -j_3 + j_1 + m_2), 0);
+
+    let mut res_cg = 0.0;
+    for k in k_min .. k_max + 1 {
+        let k_1 = factorial::factorial(j_1 + j_2 - j_3 - k);
+        let k_2 = factorial::factorial(j_1 - m_1 - k);
+        let k_3 = factorial::factorial(j_2 + m_2- k);
+        let k_4 = factorial::factorial(j_3 - j_2 + m_1 + k);
+        let k_5 = factorial::factorial(j_3 - j_1 - m_2 + k);
+
+        res_cg += sign(k as i32)/(factorial::factorial(k) * k_1 * k_2 * k_3 * k_4 * k_5);
+    }
+
+    delta(j_1, j_2, j_3).powf(0.5) * s * res_cg 
+}
 
 /// calculate Clebsch-Gordan coefficient by binomial 
 pub fn calc_cg_binomial_raw(j_1: i64, j_2: i64, j_3: i64,
                             m_1: i64, m_2: i64, m_3: i64) -> f64 { 
 
-        if is_triangle(j_1, j_2, j_3, m_1, m_2, m_3) {
-            return 0.0;
-        }
+    if is_triangle(j_1, j_2, j_3, m_1, m_2, m_3){
+        return 0.0;
+    }
 
-        let s = (2.0 * (j_3 as f64) + 1.0)/((2.0 *(j_1 as f64) + 1.0 ) * (2.0 * (j_2 as f64) + 1.0)).powf(0.5); 
+    let s = (2.0 * (j_3 as f64) + 1.0)/((2.0 *(j_1 as f64) + 1.0 ) * (2.0 * (j_2 as f64) + 1.0)).powf(0.5); 
 
-        let numerator = binomial::binomial(j_1 + j_2 + j_3 + 1, j_1 + j_2 - j_3) 
-            * binomial::binomial(2*j_3, j_3 + m_3);
-        let denominator = binomial::binomial(j_1 + j_2 + j_3 + 1, j_1 - j_2 + j_3) 
-            * binomial::binomial(j_1 + j_2 + j_3 + 1, j_2 - j_1 + j_3)
-            * binomial::binomial(2*j_1, j_1 + m_1) * binomial::binomial(2*j_2, j_2 + m_2); 
-       
-        let k_max = std::cmp::min(std::cmp::min(j_1 + j_2 - j_3, j_1 - m_1), j_2 + m_2);
-        let k_min = std::cmp::max(std::cmp::max(-j_3 + j_2 - m_1, -j_3 + j_1 + m_2), 0);
+    let numerator = binomial::binomial(j_1 + j_2 + j_3 + 1, j_1 + j_2 - j_3) 
+        * binomial::binomial(2*j_3, j_3 + m_3);
 
-        let mut res_cg = 0.0;
-        for k in k_min .. k_max + 1 {
-            res_cg += sign(k as i32) 
-                *binomial::binomial(j_1 + j_2 - j_3, k)
-                *binomial::binomial(j_3 + m_3, j_2 + m_2 - k)
-                *binomial::binomial(j_3 - m_3, j_1 - m_1 - k);
-        }
+    let denominator = binomial::binomial(j_1 + j_2 + j_3 + 1, j_1 - j_2 + j_3) 
+        * binomial::binomial(j_1 + j_2 + j_3 + 1, j_2 - j_1 + j_3)
+        * binomial::binomial(2*j_1, j_1 + m_1) * binomial::binomial(2*j_2, j_2 + m_2); 
 
-        s * (numerator / denominator).powf(0.5) * res_cg 
+    let k_max = std::cmp::min(std::cmp::min(j_1 + j_2 - j_3, j_1 - m_1), j_2 + m_2);
+    let k_min = std::cmp::max(std::cmp::max(-j_3 + j_2 - m_1, -j_3 + j_1 + m_2), 0);
+
+    let mut res_cg = 0.0;
+    for k in k_min .. k_max + 1 {
+        res_cg += sign(k as i32) 
+            *binomial::binomial(j_1 + j_2 - j_3, k)
+            *binomial::binomial(j_3 + m_3, j_2 + m_2 - k)
+            *binomial::binomial(j_3 - m_3, j_1 - m_1 - k);
+    }
+
+    s * (numerator / denominator).powf(0.5) * res_cg 
 }
