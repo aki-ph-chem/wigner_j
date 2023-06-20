8a98b40e1d0176179fb8e7341a431bc946717b82
aki <aki@mymail.com>
2023-06-20 11:02:08 +0900

    add test between cg-coefficient by factorial and cg-coefficient by binomial

--- naive_cg_coefficient/tests/test_cg_coefficient.rs
+++ naive_cg_coefficient/tests/test_cg_coefficient.rs
@@ -33,3 +33,20 @@ fn is_same_c_impl() {
     };
     assert!((cg_2.calc_value() - res_cg_2_c).abs() < std::f64::EPSILON);
 }
+
+#[test]
+fn is_same_fact() {
+    let cg_1 = cg::CGCoefficient::new(2,2,3,2,5,4);
+    cg_1.show_list();
+    let error =  (cg_1.calc_value() - cg_1.calc_value_binomial()).abs();
+    println!("error_cg_1 = {}", error);
+    assert!( error < std::f64::EPSILON);
+
+
+    let cg_2 = cg::CGCoefficient::new(2,1,1,1,3,2);
+    cg_2.show_list();
+    let error =  (cg_2.calc_value() - cg_2.calc_value_binomial()).abs();
+    println!("error_cg_2 = {}", error);
+    assert!( error < std::f64::EPSILON);
+
+}
